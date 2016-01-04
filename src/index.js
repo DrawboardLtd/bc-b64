b5818e3d7b90a3353f107082793956376c2546fc
Ben Flanagan <benjamin@jamindesigns.com>
2016-01-04 13:12:31 +1100

    add module code, unit tests for `fromHex` and `fromB64`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,129 @@
-export var bob = x => x * x
+if (!window) var window = {};
+if (!window.atob) {
+  var tableStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+  var table = tableStr.split('');
+
+  window.atob = function (base64) {
+    if (/(=[^=]+|={3,})$/.test(base64)) throw new Error('String contains an invalid character');
+    base64 = base64.replace(/=/g, '');
+    var n = base64.length & 3;
+    if (n === 1) throw new Error('String contains an invalid character');
+    for (var i = 0, j = 0, len = base64.length / 4, bin = []; i < len; ++i) {
+      var a = tableStr.indexOf(base64[j++] || 'A');
+      var b = tableStr.indexOf(base64[j++] || 'A');
+      var c = tableStr.indexOf(base64[j++] || 'A');
+      var d = tableStr.indexOf(base64[j++] || 'A');
+      if ((a | b | c | d) < 0) throw new Error('String contains an invalid character');
+      bin[bin.length] = ((a << 2) | (b >> 4)) & 255;
+      bin[bin.length] = ((b << 4) | (c >> 2)) & 255;
+      bin[bin.length] = ((c << 6) | d) & 255;
+    }
+    return String.fromCharCode.apply(null, bin).substr(0, bin.length + n - 4);
+  };
+
+  window.btoa = function (bin) {
+    for (var i = 0, j = 0, len = bin.length / 3, base64 = []; i < len; ++i) {
+      var a = bin.charCodeAt(j++);
+      var b = bin.charCodeAt(j++);
+      var c = bin.charCodeAt(j++);
+      if ((a | b | c) > 255) throw new Error('String contains an invalid character');
+      base64[base64.length] = table[a >> 2] + table[((a << 4) & 63) | (b >> 4)] +
+                              (isNaN(b) ? '=' : table[((b << 2) & 63) | (c >> 6)]) +
+                              (isNaN(b + c) ? '=' : table[c & 63]);
+    }
+    return base64.join('');
+  };
+}
+
+var h2b = (str) => {
+  return window.btoa(
+    String.fromCharCode.apply(
+      null,
+      str.replace(/\r|\n/g, '').replace(/([\da-fA-F]{2}) ?/g, '0x$1 ').replace(/ +$/, '').split(' ')
+    )
+  ).replace(/\=\=/g, '');
+}
+
+var b2h = (str) => {
+  for (var i = 0, bin = window.atob(str.replace(/[ \r\n]+$/, '')), hex = []; i < bin.length; ++i) {
+    var tmp = bin.charCodeAt(i).toString(16);
+    if (tmp.length === 1) tmp = '0' + tmp;
+    hex[hex.length] = tmp;
+  }
+  return hex.join(' ');
+}
+
+var dh2b = (hex) => {
+  hex = hex.replace(/\-/g, '').match(/.{1,2}/g).join(' '); // fix hex formatting before conversion
+  return h2b(hex).replace(/\=\=/g, '').replace(/\//g, '-'); // convert to b64, tweak b64 formatting for use in a URL
+}
+
+var b2dh = (b64) => {
+  b64 = b64.replace(/\-/g, '/');
+  var spacedHex = b2h(b64).split(' ');
+  spacedHex.splice(10, 0, '-');
+  spacedHex.splice(8, 0, '-');
+  spacedHex.splice(6, 0, '-');
+  spacedHex.splice(4, 0, '-');
+  return spacedHex.join('');
+}
+
+var h2dh = (str) => {
+  str = normalizeHex(str)
+  var spacedHex = str.split(' ');
+  spacedHex.splice(10, 0, '-');
+  spacedHex.splice(8, 0, '-');
+  spacedHex.splice(6, 0, '-');
+  spacedHex.splice(4, 0, '-');
+  return spacedHex.join('');
+}
+
+var normalizeHex = (str) => {
+  return str
+    .replace(/\-/g, '')
+    .replace(/\ /g, '')
+    .replace(/\r|\n/g, '')
+    .replace(/([\da-fA-F]{2}) ?/g, '$1 ')
+    .replace(/ +$/, '')
+}
+
+var normalizeB64 = (str) => {
+  return str.replace(/\=/g, '').replace(/\-/g, '/')
+}
+
+var hexToPanObj = (str) => {
+  return {
+    hex: str.replace(/\ /g, ''),
+    dhex: h2dh(str),
+    b64Url: h2b(str).replace(/\//g, '-')
+  }
+}
+
+var fromHex = (str) => {
+  return hexToPanObj(normalizeHex(str))
+}
+
+var fromB64 = (str) => {
+  return fromHex(b2h(normalizeB64(str)))
+}
+
+export var Bc64 = {
+  h2b: (str) => {
+    console.warn('`h2b` is deprecated as a public method of base64Service. See `fromHex` instead.')
+    return h2b(str)
+  },
+  b2h: (str) => {
+    console.warn('`b2h` is deprecated as a public method of base64Service. See `fromB64` instead.')
+    return b2h(str)
+  },
+  dh2b: (str) => {
+    console.warn('`dh2b` is deprecated as a public method of base64Service. See `fromHex` instead.')
+    return dh2b(str)
+  },
+  b2dh: (str) => {
+    console.warn('`b2dh` is deprecated as a public method of base64Service. See `fromB64` instead.')
+    return b2dh(str)
+  },
+  fromHex,
+  fromB64,
+}
--- src/index_test.js
+++ src/index_test.js
@@ -1,20 +1,85 @@
-import { bob } from "./index"
+import { Bc64 } from "./index"
 import { equal } from "assert"
-
+var testData = [
+  {dhex: '1c76ca36-320a-472f-b8b9-2ceb26b6a041', hex: '1c76ca36320a472fb8b92ceb26b6a041', hexs: '1c 76 ca 36 32 0a 47 2f b8 b9 2c eb 26 b6 a0 41', b64: 'HHbKNjIKRy+4uSzrJragQQ', b64Url: 'HHbKNjIKRy+4uSzrJragQQ'},
+  {dhex: '4d26231b-399b-4efd-a4f7-920213a04f6c', hex: '4d26231b399b4efda4f7920213a04f6c', hexs: '4d 26 23 1b 39 9b 4e fd a4 f7 92 02 13 a0 4f 6c', b64: 'TSYjGzmbTv2k95ICE6BPbA', b64Url: 'TSYjGzmbTv2k95ICE6BPbA'},
+  {dhex: '36bd108b-f65a-4853-9cd8-3c7dbb40cfd5', hex: '36bd108bf65a48539cd83c7dbb40cfd5', hexs: '36 bd 10 8b f6 5a 48 53 9c d8 3c 7d bb 40 cf d5', b64: 'Nr0Qi/ZaSFOc2Dx9u0DP1Q', b64Url: 'Nr0Qi-ZaSFOc2Dx9u0DP1Q'},
+]
 describe("index", () => {
-  describe("#bob", () => {
+  describe("#Bc64", () => {
+    describe("fromHex", () => {
+      function doTest (data) {
+        // provide hex : expect hex, dhex, b64Url
+        it(`fromHex(${data.hex}).hex => ${data.hex}`, () => {
+          equal(Bc64.fromHex(data.hex).hex, data.hex)
+        })
+        it(`fromHex(${data.hex}).dhex => ${data.dhex}`, () => {
+          equal(Bc64.fromHex(data.hex).dhex, data.dhex)
+        })
+        it(`fromHex(${data.hex}).b64Url => ${data.b64Url}`, () => {
+          equal(Bc64.fromHex(data.hex).b64Url, data.b64Url)
+        })
 
-    it("bob(1) => 1", () => {
-      equal(bob(1), 1)
+        // provide dhex : expect hex, dhex, b64Url
+        it(`fromHex(${data.dhex}).hex => ${data.hex}`, () => {
+          equal(Bc64.fromHex(data.dhex).hex, data.hex)
+        })
+        it(`fromHex(${data.dhex}).dhex => ${data.dhex}`, () => {
+          equal(Bc64.fromHex(data.dhex).dhex, data.dhex)
+        })
+        it(`fromHex(${data.dhex}).b64Url => ${data.b64Url}`, () => {
+          equal(Bc64.fromHex(data.dhex).b64Url, data.b64Url)
+        })
+
+        // provide hexs : expect hex, dhex, b64Url
+        it(`fromHex(${data.hexs}).hex => ${data.hex}`, () => {
+          equal(Bc64.fromHex(data.hexs).hex, data.hex)
+        })
+        it(`fromHex(${data.hexs}).dhex => ${data.dhex}`, () => {
+          equal(Bc64.fromHex(data.hexs).dhex, data.dhex)
+        })
+        it(`fromHex(${data.hexs}).b64Url => ${data.b64Url}`, () => {
+          equal(Bc64.fromHex(data.hexs).b64Url, data.b64Url)
+        })
+      }
+      testData.forEach(doTest)
     })
+    describe("fromB64", () => {
+      function doTest (data) {
+        // provide b64 : expect hex, dhex, b64Url
+        it(`fromB64(${data.b64}).hex => ${data.hex}`, () => {
+          equal(Bc64.fromB64(data.b64).hex, data.hex)
+        })
+        it(`fromB64(${data.b64}).dhex => ${data.dhex}`, () => {
+          equal(Bc64.fromB64(data.b64).dhex, data.dhex)
+        })
+        it(`fromB64(${data.b64}).b64Url => ${data.b64Url}`, () => {
+          equal(Bc64.fromB64(data.b64).b64Url, data.b64Url)
+        })
 
-    it("bob(2) => 4", () => {
-      equal(bob(2), 4)
+        // provide b64Url : expect hex, dhex, b64Url
+        it(`fromB64(${data.b64Url}).hex => ${data.hex}`, () => {
+          equal(Bc64.fromB64(data.b64Url).hex, data.hex)
+        })
+        it(`fromB64(${data.b64Url}).dhex => ${data.dhex}`, () => {
+          equal(Bc64.fromB64(data.b64Url).dhex, data.dhex)
+        })
+        it(`fromB64(${data.b64Url}).b64Url => ${data.b64Url}`, () => {
+          equal(Bc64.fromB64(data.b64Url).b64Url, data.b64Url)
+        })
+
+        // provide b64+'==' : expect hex, dhex, b64Url
+        it(`fromB64(${data.b64}==).hex => ${data.hex}`, () => {
+          equal(Bc64.fromB64(data.b64+'==').hex, data.hex)
+        })
+        it(`fromB64(${data.b64}==).dhex => ${data.dhex}`, () => {
+          equal(Bc64.fromB64(data.b64+'==').dhex, data.dhex)
+        })
+        it(`fromB64(${data.b64}==).b64Url => ${data.b64Url}`, () => {
+          equal(Bc64.fromB64(data.b64+'==').b64Url, data.b64Url)
+        })
+      }
+      testData.forEach(doTest)
     })
-
-    it("bob(3) => 9", () => {
-      equal(bob(3), 9)
-    })
-
   })
 })
