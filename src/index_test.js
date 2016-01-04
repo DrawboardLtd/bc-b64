2dcde1900f4bf2a97e28f3a4c4b5588fbb9e192f
Ben Flanagan <benjamin@jamindesigns.com>
2016-01-04 16:18:22 +1100

    tidy up exports & add partially applied function `deprecate`

--- src/index_test.js
+++ src/index_test.js
@@ -1,4 +1,4 @@
-import { Bc64 } from "./index"
+import { fromHex, fromB64 } from "./index"
 import { equal } from "assert"
 var testData = [
   {dhex: '1c76ca36-320a-472f-b8b9-2ceb26b6a041', hex: '1c76ca36320a472fb8b92ceb26b6a041', hexs: '1c 76 ca 36 32 0a 47 2f b8 b9 2c eb 26 b6 a0 41', b64: 'HHbKNjIKRy+4uSzrJragQQ', b64Url: 'HHbKNjIKRy+4uSzrJragQQ'},
@@ -11,35 +11,35 @@ describe("index", () => {
       function doTest (data) {
         // provide hex : expect hex, dhex, b64Url
         it(`fromHex(${data.hex}).hex => ${data.hex}`, () => {
-          equal(Bc64.fromHex(data.hex).hex, data.hex)
+          equal(fromHex(data.hex).hex, data.hex)
         })
         it(`fromHex(${data.hex}).dhex => ${data.dhex}`, () => {
-          equal(Bc64.fromHex(data.hex).dhex, data.dhex)
+          equal(fromHex(data.hex).dhex, data.dhex)
         })
         it(`fromHex(${data.hex}).b64Url => ${data.b64Url}`, () => {
-          equal(Bc64.fromHex(data.hex).b64Url, data.b64Url)
+          equal(fromHex(data.hex).b64Url, data.b64Url)
         })
 
         // provide dhex : expect hex, dhex, b64Url
         it(`fromHex(${data.dhex}).hex => ${data.hex}`, () => {
-          equal(Bc64.fromHex(data.dhex).hex, data.hex)
+          equal(fromHex(data.dhex).hex, data.hex)
         })
         it(`fromHex(${data.dhex}).dhex => ${data.dhex}`, () => {
-          equal(Bc64.fromHex(data.dhex).dhex, data.dhex)
+          equal(fromHex(data.dhex).dhex, data.dhex)
         })
         it(`fromHex(${data.dhex}).b64Url => ${data.b64Url}`, () => {
-          equal(Bc64.fromHex(data.dhex).b64Url, data.b64Url)
+          equal(fromHex(data.dhex).b64Url, data.b64Url)
         })
 
         // provide hexs : expect hex, dhex, b64Url
         it(`fromHex(${data.hexs}).hex => ${data.hex}`, () => {
-          equal(Bc64.fromHex(data.hexs).hex, data.hex)
+          equal(fromHex(data.hexs).hex, data.hex)
         })
         it(`fromHex(${data.hexs}).dhex => ${data.dhex}`, () => {
-          equal(Bc64.fromHex(data.hexs).dhex, data.dhex)
+          equal(fromHex(data.hexs).dhex, data.dhex)
         })
         it(`fromHex(${data.hexs}).b64Url => ${data.b64Url}`, () => {
-          equal(Bc64.fromHex(data.hexs).b64Url, data.b64Url)
+          equal(fromHex(data.hexs).b64Url, data.b64Url)
         })
       }
       testData.forEach(doTest)
@@ -48,35 +48,35 @@ describe("index", () => {
       function doTest (data) {
         // provide b64 : expect hex, dhex, b64Url
         it(`fromB64(${data.b64}).hex => ${data.hex}`, () => {
-          equal(Bc64.fromB64(data.b64).hex, data.hex)
+          equal(fromB64(data.b64).hex, data.hex)
         })
         it(`fromB64(${data.b64}).dhex => ${data.dhex}`, () => {
-          equal(Bc64.fromB64(data.b64).dhex, data.dhex)
+          equal(fromB64(data.b64).dhex, data.dhex)
         })
         it(`fromB64(${data.b64}).b64Url => ${data.b64Url}`, () => {
-          equal(Bc64.fromB64(data.b64).b64Url, data.b64Url)
+          equal(fromB64(data.b64).b64Url, data.b64Url)
         })
 
         // provide b64Url : expect hex, dhex, b64Url
         it(`fromB64(${data.b64Url}).hex => ${data.hex}`, () => {
-          equal(Bc64.fromB64(data.b64Url).hex, data.hex)
+          equal(fromB64(data.b64Url).hex, data.hex)
         })
         it(`fromB64(${data.b64Url}).dhex => ${data.dhex}`, () => {
-          equal(Bc64.fromB64(data.b64Url).dhex, data.dhex)
+          equal(fromB64(data.b64Url).dhex, data.dhex)
         })
         it(`fromB64(${data.b64Url}).b64Url => ${data.b64Url}`, () => {
-          equal(Bc64.fromB64(data.b64Url).b64Url, data.b64Url)
+          equal(fromB64(data.b64Url).b64Url, data.b64Url)
         })
 
         // provide b64+'==' : expect hex, dhex, b64Url
         it(`fromB64(${data.b64}==).hex => ${data.hex}`, () => {
-          equal(Bc64.fromB64(data.b64+'==').hex, data.hex)
+          equal(fromB64(data.b64+'==').hex, data.hex)
         })
         it(`fromB64(${data.b64}==).dhex => ${data.dhex}`, () => {
-          equal(Bc64.fromB64(data.b64+'==').dhex, data.dhex)
+          equal(fromB64(data.b64+'==').dhex, data.dhex)
         })
         it(`fromB64(${data.b64}==).b64Url => ${data.b64Url}`, () => {
-          equal(Bc64.fromB64(data.b64+'==').b64Url, data.b64Url)
+          equal(fromB64(data.b64+'==').b64Url, data.b64Url)
         })
       }
       testData.forEach(doTest)
